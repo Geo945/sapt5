4195f942b15c2debc6ae9c45c1fe3e056a399081
raresmatei <73436620+raresmatei@users.noreply.github.com>
2021-03-14 15:43:14 +0200

    Update src/loose/oose/fis/lab/student/manager/model/Student.java
    Title of the project changed to FIS Student Manager
    
    Co-authored-by: Geo945 <80351427+Geo945@users.noreply.github.com>

--- src/loose/oose/fis/lab/student/manager/model/Student.java
+++ src/loose/oose/fis/lab/student/manager/model/Student.java
@@ -16,6 +16,7 @@ public class Student {
     }
 
     @Override
+    primaryStage.setTitle("FIS - Student Manager");
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
